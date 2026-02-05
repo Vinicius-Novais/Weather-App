3440845f76f61f1634193b9be5021e1126c7c5e9
Vinícius <148072300+Vinicius-Novais@users.noreply.github.com>
2026-02-05 01:12:43 -0300

    extract Weather Data

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const elements = {
   heroTextBox: document.querySelector(".hero__textbox"),
   heroButton: document.querySelector(".hero__button"),
 };
-
+const coordinates = [];
 const round = (value) => Math.round(value);
 
 // ============================= GEOLOCOCATION =======================================
 
 elements.heroButton.addEventListener("click", loadGeolocation);
+// elements.heroButton.addEventListener("click", loadWeather);
 
 function buildGeolocationURL() {
   const params = new URLSearchParams({
@@ -19,7 +20,7 @@ function buildGeolocationURL() {
   return `https://nominatim.openstreetmap.org/search?${params}`;
 }
 
-async function fetchGeolocation() {
+async function fetchGeolocationAPI() {
   const geoData = await fetch(buildGeolocationURL());
 
   if (!geoData.ok) {
@@ -31,7 +32,7 @@ async function fetchGeolocation() {
 
 async function loadGeolocation() {
   try {
-    const geoJson = await fetchGeolocation();
+    const geoJson = await fetchGeolocationAPI();
     console.log(geoJson);
     extractGeolocationData(geoJson);
   } catch (error) {
@@ -44,13 +45,83 @@ function showerror(error) {
   console.log("ERRO: " + error.message);
 }
 
-async function extractGeolocationData(rawData) {
+function extractGeolocationData(rawData) {
   const lat = rawData[0].lat;
   const lon = rawData[0].lon;
+  const place = rawData[0].name;
 
-  buildWeatherURL(lat, lon);
+  coordinates[0] = lat;
+  coordinates[1] = lon;
+
+  // const placeElement = document.querySelector(".current__city");
+
+  // placeElement.textContent = place;
+  // console.log(coordinates);
+  loadWeather();
 }
 
 // =================================== WEATHER ========================================
 
-function buildWeatherURL() {}
+function buildWeatherURL(lat, lon) {
+  const params = new URLSearchParams({
+    latitude: lat,
+    longitude: lon,
+    daily: "weather_code,temperature_2m_max,temperature_2m_min",
+    hourly: "temperature_2m,weather_code",
+    current: "temperature_2m,weather_code,relative_humidity_2m,apparent_temperature,is_day,precipitation,wind_speed_10m",
+    timezone: "auto",
+  });
+
+  console.log(`Passando URL com a lat e lon: https://api.open-meteo.com/v1/forecast?${params}`);
+  return `https://api.open-meteo.com/v1/forecast?${params}`;
+}
+
+async function fetchWeatherAPI() {
+  const weatherData = await fetch(buildWeatherURL(coordinates[0], coordinates[1]));
+
+  if (!weatherData.ok) {
+    throw Error("Erro HTTP: " + weatherData.status);
+  }
+
+  return weatherData.json();
+}
+
+async function loadWeather() {
+  try {
+    const weatherJson = await fetchWeatherAPI();
+
+    extractWeatherData(weatherJson);
+  } catch (error) {
+    console.log(error);
+    showerror();
+  }
+}
+
+function extractWeatherData(rawData) {
+  const currentData = {
+    temperature: round(rawData.current.temperature_2m),
+    feelsLike: round(rawData.current.apparent_temperature),
+    humidity: rawData.current.relative_humidity_2m,
+    wind: round(rawData.current.wind_speed_10m),
+    precipitation: round(rawData.current.precipitation),
+    isDay: rawData.current.is_day,
+  };
+
+  const current = document.querySelector(".current");
+
+  const currentElements = {
+    temperature: current.querySelector('[data-condition="temp"]'),
+    feelsLike: current.querySelector('[data-condition="feelsLike"]'),
+    humidity: current.querySelector('[data-condition="humidity"]'),
+    wind: current.querySelector('[data-condition="wind"]'),
+    precipitation: current.querySelector('[data-condition="precipitation"]'),
+  };
+
+  currentElements.temperature.textContent = currentData.temperature + "\u00B0";
+  currentElements.feelsLike.textContent = currentData.feelsLike + "\u00B0";
+  currentElements.humidity.textContent = currentData.humidity + "%";
+  currentElements.wind.textContent = currentData.wind + " km/h";
+  currentElements.precipitation.textContent = currentData.precipitation + " mm";
+
+  const dailyData = {};
+}
